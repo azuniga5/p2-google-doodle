defc471fb456718c95818f620b4b6c49d6cf379b
azuniga5 <azuniga5@vcd038841md.eastern.ewu.edu>
2017-10-16 15:01:27 -0700

    css

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 // Scripts
 
+$('#stage')
+    .animate({opacity: 0}, 800, function() {
+        $(this)
+            .css({'background-image': 'url(../img/sky.png)'})
+            .animate({opacity: 1});
+    });
+
+;
+
 
 $("#car").delay(900).animate({left: "-50px"}, {duration: 750, queue: true});
 
