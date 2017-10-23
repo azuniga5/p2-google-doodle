f0fa0f21aa9ec79455294d946f34a3612e67d4c6
azuniga5 <alejandrozun123@gmail.com>
2017-10-22 22:50:04 -0700

    timing

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 // Scripts
 
 $('#stage')
-    .animate({opacity: 0}, 800, function() {
+    .animate({opacity: 0}, 1800, function() {
         $(this)
             .css({'background-image': 'url(../img/sky.png)'})
             .animate({opacity: 1});
     });
 
-;
+; 
 
 
-$("#car").delay(900).animate({left: "-50px"}, {duration: 750, queue: true});
+$("#car").delay(2850).animate({left: "-50px"}, {duration: 1350, queue: true});
 
 
 
