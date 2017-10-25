9b3b333f2e0863b189333bb943142c51d638e287
azuniga5 <alejandrozun123@gmail.com>
2017-10-24 23:27:10 -0700

    jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
 // Scripts
 
-$('#stage')
-    .animate({opacity: 0}, 1800, function() {
+$('#stage').animate({opacity: 0}, 1800, function() {
         $(this)
             .css({'background-image': 'url(/img/clouds.png)'})
             .animate({opacity: 1});
     });
 
-; 
+
+
 
 
 $("#car").delay(2850).animate({left: "-50px"}, {duration: 1350, queue: true});
 
+$("h1").animate({opacity: 10}, 25000);
 
 
 
